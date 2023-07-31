067240e397c63db439208439191e42e4e6dca696
Iamneo <techadmin@iamneo.ai>
2023-07-31 17:45:24 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,3 +1,13 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'angularapp';
+// }
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +17,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularapp';
+  opt1:number = 0;
+  opt2:number = 0;
+  amt:number = 0;
+  res:number = 0;
+  result:string = '';
+  exchange(a:number,b:number,amt:number){
+    if(a!=null && b!=null){
+      this.result=Math.round(b/a*amt).toFixed(2);
+      return this.result;
+    }
+  }
 }
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,6 +1,24 @@
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,9 +28,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
