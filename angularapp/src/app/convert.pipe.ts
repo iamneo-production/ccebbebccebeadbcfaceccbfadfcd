c4c15e9c87a2ff93ff3eb090d71f93bcf0073802
Iamneo <techadmin@iamneo.ai>
2023-07-31 18:00:21 +0000

    Backup Commit

--- angularapp/src/app/convert.pipe.ts
+++ angularapp/src/app/convert.pipe.ts
@@ -1,26 +1,3 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { ConvertPipe } from './convert.pipe';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    ConvertPipe
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-        ReactiveFormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
-
-app.module.ts
-
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
@@ -33,3 +10,5 @@ export class ConvertPipe implements PipeTransform {
   }
 
 }
+
+
