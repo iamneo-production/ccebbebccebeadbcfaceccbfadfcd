f6ad0c2d7524a71b43f0955ebe526d41c147e53a
Iamneo <techadmin@iamneo.ai>
2023-07-31 18:28:06 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -9,58 +9,38 @@
 //   title = 'angularapp';
 // }
 
-// import { Component } from '@angular/core';
-// import { FormControl, FormGroup } from '@angular/forms';
-// import { ConvertPipe } from './convert.pipe';
-
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.scss']
-// })
-// export class AppComponent {
-//   title = 'currency_convertor';
-//   inputForm: any = new FormGroup({
-//     fromCurrency: new FormControl({ value: '', disabled: false }),
-//     toCurrency: new FormControl({ value: '', disabled: false }),
-//     amount: new FormControl({ value: '', disabled: false }),
-//     submitButton: new FormControl({ value: '', disabled: false }),
-//     resValue: new FormControl({ value: '', disabled: false })
-//   })
-//   currency = [{ "id": "USD", "value": 1.126735 },
-//   { "id": "GBP", "value": 0.876893 },
-//   { "id": "INR", "value": 79.677056 }
-//   ]
-//   ngOninit() {
-
-//   }
-//   submitButton() {
-//     let val = new ConvertPipe().transform(this.inputForm.value.fromCurrency,this.inputForm.value.toCurrency,this.inputForm.value.amount )
-//     val = String(val) + '.00'
-//     this.inputForm.value.resValue = val
-//   }
-// }
-
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ConvertPipe } from './convert.pipe';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularapp';
-  opt1:number;
-  opt2:number;
-  amt:number;
-  res:number;
-  result:string;
-  exchange(a:number,b:number,amt:number){
-    if(a!=null && b!=null){
-      this.result=Math.round(b/a*amt).toFixed(2);
-      return this.result;
-    }
+  title = 'currency_convertor';
+  inputForm: any = new FormGroup({
+    fromCurrency: new FormControl({ value: '', disabled: false }),
+    toCurrency: new FormControl({ value: '', disabled: false }),
+    amount: new FormControl({ value: '', disabled: false }),
+    submitButton: new FormControl({ value: '', disabled: false }),
+    resValue: new FormControl({ value: '', disabled: false })
+  })
+  currency = [{ "id": "USD", "value": 1.126735 },
+  { "id": "GBP", "value": 0.876893 },
+  { "id": "INR", "value": 79.677056 }
+  ]
+  ngOninit() {
+
+  }
+  submitButton() {
+    let val = new ConvertPipe().transform(this.inputForm.value.fromCurrency,this.inputForm.value.toCurrency,this.inputForm.value.amount )
+    val = String(val) + '.00'
+    this.inputForm.value.resValue = val
   }
 }
+
+
 
 
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -15,46 +15,28 @@
 //   providers: [],
 //   bootstrap: [AppComponent]
 // })
-// import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
-// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-// import { AppComponent } from './app.component';
-// import { ConvertPipe } from './convert.pipe';
-
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     ConvertPipe
-//   ],
-//   imports: [
-//     BrowserModule,
-//     FormsModule,
-//         ReactiveFormsModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-
-
-
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ConvertPipe } from './convert.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConvertPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    FormsModule,
+        ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+
 
