79ef3eecb7aace37d2dc8ab8c3e1b00fbeb9a039
Iamneo <techadmin@iamneo.ai>
2023-07-31 18:25:22 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -15,25 +15,46 @@
 //   providers: [],
 //   bootstrap: [AppComponent]
 // })
-import { NgModule } from '@angular/core';
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { AppComponent } from './app.component';
+// import { ConvertPipe } from './convert.pipe';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     ConvertPipe
+//   ],
+//   imports: [
+//     BrowserModule,
+//     FormsModule,
+//         ReactiveFormsModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
+
+
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ConvertPipe } from './convert.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ConvertPipe
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-        ReactiveFormsModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
 
